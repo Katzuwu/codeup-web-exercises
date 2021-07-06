72817d741751a968c9d8a0812ecbd43f73939530
Ian Arellano <ian.g.arellano01@gmail.com>
2021-07-06 15:20:56 -0500

    add external.js and external_js.html

---
+++ js/external.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// console.log("Hello from external JavaScript!")
+//
+// alert("Welcome to my website, Master uwu");
+//
+// var favoriteColor = prompt("What is your favorite colour, Master?");
+// alert("Wow Master, " + favoriteColor + " is also my favorite color!");
+
+// var movieRentalRate = 3;
+// var mermaidRentalDays = prompt("How long would you like to rent The Little Mermaid for?");
+// var bearRentalDays = prompt("How long would you like to rent Brother Bear for?");
+// var herculesRentalDays = prompt("How long would you like to rent Hercules for?");
+// function totalCost() {
+// 	return (bearRentalDays * movieRentalRate) + (mermaidRentalDays * movieRentalRate) + (herculesRentalDays * movieRentalRate);
+// }
+// alert("Okay! It'll be $" + totalCost() + " !");
+
+// var googlePay = 400;
+// var amazonPay = 380;
+// var facebookPay = 350;
+//
+// var googleHours = prompt("How long did you work at Google for?");
+// var amazonHours = prompt("How long did you work at Amazon for?");
+// var facebookHours = prompt("How long did you work for Facebook for?");
+//
+// function totalPay() {
+// 	return (facebookPay * facebookHours) + (amazonPay * amazonHours) + (googlePay * googleHours);
+// }
+//
+// alert("Okay! You made $" + totalPay() + "!");
+
+// var classFull = confirm("Is the class full? If yes, click \'OK\'. If not, click \'Cancel\'.");
+// var scheduleConflict = confirm("Is there a schedule conflict? If yes, click \'OK\'. If not, click \'Cancel\'.")
+//
+// if (classFull === true || scheduleConflict === true) {
+// 	alert("Sorry, you cannot enroll.");
+// } else {
+// 	alert("Congrats! You can enroll!");
+// }
+
+// var offerExpired = confirm("Is your offer expired? Click \'Cancel\' if not.");
+// var premiumMember = confirm("Are you a premium member? Click \'Cancel\' if not.");
+// var itemCount = confirm("Do you have more than 2 items in your cart? Click \'Cancel\' if you do not.");
+//

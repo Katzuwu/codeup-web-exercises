6aa4c45cf8ec32758bd219c4fb6caef522a41395
Ian Arellano <ian.g.arellano01@gmail.com>
2021-07-12 13:31:02 -0500

    solved some bonus functions

--- js/bonus_function_problems.js
+++ js/bonus_function_problems.js
@@ -1,5 +1,45 @@
 "use strict";
 
 function isOdd(num) {
-	return (num % 2) == 1;
+	return (num % 2) === 1;
 }
+
+function isEven(num) {
+	return num % 2 === 0;
+}
+
+function identity(input) {
+	return typeof input;
+}
+
+function isFive(input) {
+	return input === 5;
+}
+
+function addFive(num) {
+	return num + 5;
+}
+
+function isThree(num) {
+	return num === 3;
+}
+
+function isMultipleOfThree(num){
+	return num % 3 === 0;
+}
+
+function isMultipleOfThreeAndFive(input){
+	return input % 3 === 0 && input % 5 === 0;
+}
+
+function isMultipleOf(target, n) {
+	return target % n === 0;
+}
+
+function isTrue(input){
+	if(typeof input === 'boolean'){
+		return input;
+	}
+	return false;
+}
+

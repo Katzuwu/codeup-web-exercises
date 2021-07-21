52fd57b846b0cb746f05f5def414725a129090fe
Ian Arellano <ian.g.arellano01@gmail.com>
2021-07-21 12:33:42 -0500

    finally corrected break and continue.js to catch any input outside of the criteria

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -17,10 +17,10 @@
 
 do {
 	var userNumber = parseFloat(prompt("Master, will you enter an odd number between 1 and 50 to skip for me?"));
-	if (userNumber % 2 !== 0) {
+	if ((userNumber % 2 !== 0) && (userNumber < 50) && (userNumber > 1) && (!isNaN(userNumber))) {
 		break;
 	}
-} while (userNumber % 2 === 0) {
+} while (userNumber % 2 === 0 || (userNumber > 50) || (userNumber < 1) || (isNaN(userNumber))) {
 	console.log("Master, your number to skip is: " + userNumber);
 		for (var i = 1; i < 50; i++) {
 			if (i % 2 === 0) {

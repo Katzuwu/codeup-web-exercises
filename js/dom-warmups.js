ec96a8fa2dcb6f5e9c8912c0aa0b1587cd6cc385
Ian Arellano <ian.g.arellano01@gmail.com>
2021-08-17 09:27:16 -0500

    merged dom-warmups.js into dom-warmup.html

--- js/dom-warmups.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-var header = document.getElementById('goodMorning');
-var afternoonButton = document.getElementById('afternoon');
-afternoonButton.addEventListener('click', function (){
-	header.innerHTML = 'Good Afternoon, Draco!';
-});
